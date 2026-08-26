9f53fb17f5d6803ab78f046e8ad27b6d5ba4ff87
Dzmitryi Kharlanau <dkharlanau@gmail.com>
2026-08-26 21:20:43 +0300

    release: expose PR and interoperability API for v0.5

--- src/mapping_as_code/__init__.py
+++ src/mapping_as_code/__init__.py
@@ -6,28 +6,40 @@ from .adapters import (
     to_transformation_graph,
     to_visual_workbench,
 )
+from .annotations import github_annotations
 from .artifacts import catalog_html, catalog_markdown, release_bundle, traceability_matrix
+from .change_projection import to_enterprise_change_transition
 from .core import Diagnostic, diff_documents, lineage_graph, lineage_mermaid, validate_document
 from .governance import breaking_change_report, policy_diagnostics, quality_scorecard, validation_report
+from .graph_exports import lineage_cypher, lineage_graphml
+from .interface_binding import bind_interface_contract
 from .io import load_document
+from .review import review_markdown, review_report
 from .tabular import ImportErrorDetail, import_rows, import_tabular
 
 __all__ = [
     "Diagnostic",
     "ImportErrorDetail",
+    "bind_interface_contract",
     "breaking_change_report",
     "catalog_html",
     "catalog_markdown",
     "diff_documents",
+    "github_annotations",
     "import_rows",
     "import_tabular",
+    "lineage_cypher",
     "lineage_graph",
+    "lineage_graphml",
     "lineage_mermaid",
     "load_document",
     "policy_diagnostics",
     "quality_scorecard",
     "release_bundle",
+    "review_markdown",
+    "review_report",
     "to_enterprise_change_graph",
+    "to_enterprise_change_transition",
     "to_reconciliation",
     "to_transformation_graph",
     "to_visual_workbench",
@@ -36,4 +48,4 @@ __all__ = [
     "validation_report",
 ]
 
-__version__ = "0.4.0"
+__version__ = "0.5.0"
