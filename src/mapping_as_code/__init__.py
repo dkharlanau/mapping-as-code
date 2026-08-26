1542f579cbf380cc8cf47e3561cce01a9cd4a620
Dzmitryi Kharlanau <dkharlanau@gmail.com>
2026-08-26 20:42:37 +0300

    feat: expose v0.3 interoperability projections

--- src/mapping_as_code/__init__.py
+++ src/mapping_as_code/__init__.py
@@ -1,5 +1,11 @@
 """Mapping as Code public API."""
 
+from .adapters import (
+    to_enterprise_change_graph,
+    to_reconciliation,
+    to_transformation_graph,
+    to_visual_workbench,
+)
 from .core import Diagnostic, diff_documents, lineage_graph, lineage_mermaid, validate_document
 from .io import load_document
 from .tabular import ImportErrorDetail, import_rows, import_tabular
@@ -13,7 +19,11 @@ __all__ = [
     "lineage_graph",
     "lineage_mermaid",
     "load_document",
+    "to_enterprise_change_graph",
+    "to_reconciliation",
+    "to_transformation_graph",
+    "to_visual_workbench",
     "validate_document",
 ]
 
-__version__ = "0.2.0"
+__version__ = "0.3.0"
