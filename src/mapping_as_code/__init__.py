10dcf2844605fc77cdfff67aaf2b09e653136a88
Dzmitryi Kharlanau <dkharlanau@gmail.com>
2026-08-26 20:34:44 +0300

    feat: expose workbook import API in v0.2

--- src/mapping_as_code/__init__.py
+++ src/mapping_as_code/__init__.py
@@ -2,14 +2,18 @@
 
 from .core import Diagnostic, diff_documents, lineage_graph, lineage_mermaid, validate_document
 from .io import load_document
+from .tabular import ImportErrorDetail, import_rows, import_tabular
 
 __all__ = [
     "Diagnostic",
+    "ImportErrorDetail",
     "diff_documents",
+    "import_rows",
+    "import_tabular",
     "lineage_graph",
     "lineage_mermaid",
     "load_document",
     "validate_document",
 ]
 
-__version__ = "0.1.0"
+__version__ = "0.2.0"
