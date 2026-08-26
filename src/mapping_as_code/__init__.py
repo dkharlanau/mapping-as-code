d890ab37d016a9ed9846d5a8207879c993666f6c
Dzmitryi Kharlanau <dkharlanau@gmail.com>
2026-08-26 21:02:09 +0300

    feat: expose governance and evidence public API

--- src/mapping_as_code/__init__.py
+++ src/mapping_as_code/__init__.py
@@ -6,24 +6,34 @@ from .adapters import (
     to_transformation_graph,
     to_visual_workbench,
 )
+from .artifacts import catalog_html, catalog_markdown, release_bundle, traceability_matrix
 from .core import Diagnostic, diff_documents, lineage_graph, lineage_mermaid, validate_document
+from .governance import breaking_change_report, policy_diagnostics, quality_scorecard, validation_report
 from .io import load_document
 from .tabular import ImportErrorDetail, import_rows, import_tabular
 
 __all__ = [
     "Diagnostic",
     "ImportErrorDetail",
+    "breaking_change_report",
+    "catalog_html",
+    "catalog_markdown",
     "diff_documents",
     "import_rows",
     "import_tabular",
     "lineage_graph",
     "lineage_mermaid",
     "load_document",
+    "policy_diagnostics",
+    "quality_scorecard",
+    "release_bundle",
     "to_enterprise_change_graph",
     "to_reconciliation",
     "to_transformation_graph",
     "to_visual_workbench",
+    "traceability_matrix",
     "validate_document",
+    "validation_report",
 ]
 
-__version__ = "0.3.0"
+__version__ = "0.4.0"
